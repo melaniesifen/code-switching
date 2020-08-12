e63fd3a5e179294f13c2ec178cfc385eb9ec277c
Melanie Sifen <melanie.sifen@gmail.com>
2020-08-11 21:16:49 -0500

    Update RunAnalysis.py
    Comments added

--- RunAnalysis.py
+++ RunAnalysis.py
@@ -3,7 +3,9 @@ import re
 
 my_file = input("Enter relative path of file: ")
 title = input("Enter title: ")
+# Remove all but alphanumerics and space
 title = re.sub(r"[^\w\s]", '', title)
+# Eliminate duplicate whitespaces, replace whitespace with underscore
 title = re.sub(r"\s+", '_', title)
 
 if __name__ == "__main__":
